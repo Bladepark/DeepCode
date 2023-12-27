f3dc7acd6d852b31d90d3b1f82c8d17878159adb
Young Park <minyeoungpark6929@gmail.com>
2023-12-27 12:54:54 +0900

    home, recruit xml layout complete

--- app/src/main/java/com/example/deepcode/HomeActivity.kt
+++ app/src/main/java/com/example/deepcode/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.example.deepcode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 // 5조 파이팅!!
 
@@ -9,5 +11,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val recruit = findViewById<ImageView>(R.id.btn_recruit)
+
+        recruit.setOnClickListener {
+            val intent = Intent(this, RecruitActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
