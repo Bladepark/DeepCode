74a431771f5017912d400e61268146538581c8d6
Young Park <minyeoungpark6929@gmail.com>
2023-12-28 16:10:07 +0900

    for first merge packaging complete

--- app/src/main/java/com/example/deepcode/sign/SignInActivity.kt
+++ app/src/main/java/com/example/deepcode/sign/SignInActivity.kt
@@ -1,11 +1,12 @@
-package com.example.deepcode
+package com.example.deepcode.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.deepcode.R
 
-class FollowerActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_follower)
+        setContentView(R.layout.activity_sign_in)
     }
 }
