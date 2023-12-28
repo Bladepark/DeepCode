e369d946a62ad116008c22a3245b5aa8f8a3afb8
ChooJeongHo <kd64choo@gmail.com>
2023-12-28 21:06:14 +0900

    네비게이션 추가

--- app/src/main/java/com/example/deepcode/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/deepcode/profile/ProfileActivity.kt
@@ -7,12 +7,29 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.deepcode.HomeActivity
+import com.example.deepcode.NewsActivity
 import com.example.deepcode.R
+import com.example.deepcode.RecruitActivity
 import com.example.deepcode.follow.FollowerActivity
 import com.example.deepcode.follow.FollowingActivity
 import com.example.deepcode.sign.SignInActivity
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val navHomeBtn: ImageView by lazy { findViewById(R.id.btn_home) }
+    private val navRecruitBtn: ImageView by lazy { findViewById(R.id.btn_recruit) }
+    private val navNewsBtn: ImageView by lazy { findViewById(R.id.btn_news) }
+    private val navProfileBtn: ImageView by lazy { findViewById(R.id.btn_profile) }
+
+    private val navBtns
+        get() = listOf(
+            navHomeBtn,
+            navRecruitBtn,
+            navNewsBtn,
+            navProfileBtn
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -37,6 +54,18 @@ class ProfileActivity : AppCompatActivity() {
             R.drawable.profile5
         )
         ivProfile.setImageResource(profileList.random())
+
+
+        navBtns.forEach { navBtn ->
+            navBtn.setOnClickListener {
+                when (navBtn) {
+                    navHomeBtn -> startActivity(Intent(this, HomeActivity::class.java))
+                    navRecruitBtn -> startActivity(Intent(this, RecruitActivity::class.java))
+                    navNewsBtn -> startActivity(Intent(this, NewsActivity::class.java))
+                    navProfileBtn -> startActivity(Intent(this, ProfileActivity::class.java))
+                }
+            }
+        }
 
         btnUpdate.setOnClickListener {
             val intent = Intent(this, UpdateProfileActivity::class.java)
