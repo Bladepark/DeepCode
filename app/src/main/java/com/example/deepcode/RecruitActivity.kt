80c70ccb2f1bde6839d42b648950ab64116a09ba
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2023-12-29 13:21:37 +0900

    sign_in sign_up complete

--- app/src/main/java/com/example/deepcode/RecruitActivity.kt
+++ app/src/main/java/com/example/deepcode/RecruitActivity.kt
@@ -169,7 +169,7 @@ class RecruitActivity : AppCompatActivity() {
                         val intent = Intent(this, DetailActivity::class.java)
                         intent.putExtra("itemProfile", R.drawable.ic_retple)
                         intent.putExtra("itemUserName", R.string.recuit_item6_user_name)
-                        intent.putExtra("itemImg", R.drawable.ic_threethree_img)
+                        intent.putExtra("itemImg", R.drawable.ic_peole)
                         intent.putExtra("itemText", R.string.recuit_item6_text)
                         startActivity(intent)
                     }
@@ -177,7 +177,7 @@ class RecruitActivity : AppCompatActivity() {
                         val intent = Intent(this, DetailActivity::class.java)
                         intent.putExtra("itemProfile", R.drawable.ic_10comu)
                         intent.putExtra("itemUserName", R.string.recuit_item7_user_name)
-                        intent.putExtra("itemImg", R.drawable.ic_threethree_img)
+                        intent.putExtra("itemImg", R.drawable.ic_10comuple)
                         intent.putExtra("itemText", R.string.recuit_item7_text)
                         startActivity(intent)
                     }
@@ -185,7 +185,7 @@ class RecruitActivity : AppCompatActivity() {
                         val intent = Intent(this, DetailActivity::class.java)
                         intent.putExtra("itemProfile", R.drawable.ic_china)
                         intent.putExtra("itemUserName", R.string.recuit_item8_user_name)
-                        intent.putExtra("itemImg", R.drawable.ic_threethree_img)
+                        intent.putExtra("itemImg", R.drawable.ic_people2)
                         intent.putExtra("itemText", R.string.recuit_item8_text)
                         startActivity(intent)
                     }
@@ -193,7 +193,7 @@ class RecruitActivity : AppCompatActivity() {
                         val intent = Intent(this, DetailActivity::class.java)
                         intent.putExtra("itemProfile", R.drawable.ic_cloth)
                         intent.putExtra("itemUserName", R.string.recuit_item9_user_name)
-                        intent.putExtra("itemImg", R.drawable.ic_threethree_img)
+                        intent.putExtra("itemImg", R.drawable.ic_people3)
                         intent.putExtra("itemText", R.string.recuit_item9_text)
                         startActivity(intent)
                     }
@@ -201,7 +201,7 @@ class RecruitActivity : AppCompatActivity() {
                         val intent = Intent(this, DetailActivity::class.java)
                         intent.putExtra("itemProfile", R.drawable.ic_horse)
                         intent.putExtra("itemUserName", R.string.recuit_item10_user_name)
-                        intent.putExtra("itemImg", R.drawable.ic_threethree_img)
+                        intent.putExtra("itemImg", R.drawable.ic_people4)
                         intent.putExtra("itemText", R.string.recuit_item10_text)
                         startActivity(intent)
                     }
